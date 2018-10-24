31cbc4d01d97a264def1ef40d4ed67a43af046c9
Ruben Miller <rmiller@mindresearch.org>
2018-10-24 20:11:03 -0300

    * src/lib/bundle: Fix manifest path. Move validation checks to the exported bundle function.

--- src/lib/bundle.js
+++ src/lib/bundle.js
@@ -11,35 +11,40 @@ export const bundleAssets = () => {
     }
 }
 
-export function bundleGame (gameName, path) {
-	const spawn = child_process.spawnSync;
-	// Exec the global jspm command instead of calling a library function, so we make sure of being using the correct jspm version.
-	const command = (os.platform() === 'win32') ? 'jspm.cmd' : 'jspm';
-	const res = spawn(command, ['bundle', `${path} - mind-sdk/**/*`, `${gameName}.js`]); // path : ${BUNDLE_DIRECTORY}/${gameName}/${gameName}
-	if (!res.error && res.status === 0) {
-		writeManifest(gameName);
-	}
+export function bundleGame (name, arenaKey, path, version) {
+    const validParams = [name, arenaKey, path].find(p => typeof p !== 'string' || p.length === 0) === undefined;
+    if (validParams) {
+        const spawn = child_process.spawnSync;
+        // Exec the global jspm command instead of calling a library function, so we make sure of being using the correct jspm version.
+        const command = (os.platform() === 'win32') ? 'jspm.cmd' : 'jspm';
+        const res = spawn(command, ['bundle', `${path} - mind-sdk/**/*`, `${name}.js`]); // path : ${BUNDLE_DIRECTORY}/${gameName}/${gameName}
+        if (!res.error && res.status === 0) {
+            writeManifest(name, arenaKey, path, version);
+        }
+    }
 };
 
-const writeManifest = (gameName, dir, version) => {
-    if (typeof gameName === 'string' || gameName.length > 0) {
-        const sdkVersion = getPackageJsonField('jspm.dependencies.mind-sdk');
-        const dump = `{
-            "module": "${gameName}",
-            "arenaKey": "PixiArenas/${gameName}/${gameName}",
-            "version" : "${version}",
-            "sdkBundleFile": "/pilot/sdk/mind-sdk-${sdkVersion}.js",
-            "gameBundleFile": ${createPath('/pilot/arenas', gameName, version, gameName + '.js')},
-            "assetsBaseUrl": "/pilot",
-            "systemJsConfig": {
-                "map": {
-                    "mind-sdk": "mind:mind-sdk@${sdkVersion}"
-                }
-            }
-        }`;
-
-        FS.writeFileSync(createPath(dir, `${gameName}.manifest.json`), dump); // path: ${BUNDLE_DIRECTORY}/${gameName}/${gameName}.manifest.json
+const writeManifest = (gameName, arenaKey, path, version) => {
+    if (path.endsWith('.js')) {
+        path = path.substr(0, path.length - 3);
     }
+    path = path.concat('.manifest.js');
+    const sdkVersion = getPackageJsonField('jspm.dependencies.mind-sdk');
+    const dump = `{
+        "module": "${gameName}",
+        "arenaKey": "${arenaKey}",
+        "version" : "${version}",
+        "sdkBundleFile": "/pilot/sdk/mind-sdk-${sdkVersion}.js",
+        "gameBundleFile": ${createPath('/pilot/arenas', gameName, version, gameName + '.js')},
+        "assetsBaseUrl": "/pilot",
+        "systemJsConfig": {
+            "map": {
+                "mind-sdk": "mind:mind-sdk@${sdkVersion}"
+            }
+        }
+    }`;
+
+    FS.writeFileSync(path, dump); // path: ${BUNDLE_DIRECTORY}/${gameName}/${gameName}.manifest.json
 }
 
 const getPackageJsonField = field => {
