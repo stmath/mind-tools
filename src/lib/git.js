3d07ac8a15f162aec92421eaf416bfb79c2123bc
Ruben Miller <rmiller@mindresearch.org>
2018-10-25 20:11:50 -0300

    * src/lib/git::getLastTag(): use tags.all.sort().pop() instead of tags.latest, since only show the last tag published.
    * src/lib/git::addTag: Is git().addTag not addtag.

--- src/lib/git.js
+++ src/lib/git.js
@@ -13,13 +13,13 @@ export const getTags = (dir = undefined) => {
 }
 
 export const getLastTag = (dir = undefined) => {
-    return getTags(dir).then(tags => tags.latest);
+    return getTags(dir).then(tags => tags.all.sort().pop());
 }
 
 export const addTag = (tagname, dir = undefined) => {
     return new Promise((resolve, reject) => {
         if ((typeof tagname === 'string' && tagname.length > 0) || typeof tagname === 'number') {
-            git(dir).addtag(String(tagname), (err, tags) => {
+            git(dir).addTag(String(tagname), (err, tags) => {
                 resolve(tags);
             });
         } else {
