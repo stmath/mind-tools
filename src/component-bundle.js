9f18ae59dc64f8731ca39f7a14d1cd88fde87e6f
Bryce Walters <bwalters@mindresearch.org>
2019-09-24 14:31:16 -0700

    Removing options that will not be available for component-bundle Adding a function to find the latest tag on the repo in the case that a version is not supplied.

--- src/component-bundle.js
+++ src/component-bundle.js
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 import {bundleComponents, uploadBundleComponents, setLogHandler} from './lib/bundle';
-import {addTag} from './lib/git';
+import {addTag, getTags, mostRecentTag} from './lib/git';
 import {mkdir} from './lib/common/file';
 import commandLineArgs from 'command-line-args';
 
 const optionDefinitions = [
-	{ name: 'upload', alias: 'u', type: Boolean },
 	{ name: 'dest', alias: 'd', type: String, defaultValue: 'components/' },
-	{ name: 'version', type: String },
-	{ name: 'tag', type: Boolean},
+	{ name: 'version', type: String }
 ];
 
 const options = commandLineArgs(optionDefinitions);
 const log = console.log;
 
 setLogHandler(log);
-let version = options.version;
 mkdir(options.dest);
-log(`Bundling Components to ${options.dest}${version}/`);
-let success = bundleComponents(version, {sourceMap: options.sourceMap, noMinify: options.noMinify});
-if (!success) new Error('Error while bundling Components.');
-if (options.upload) {
-	log('Upload bundle: ' + version);
-	uploadBundleComponents(version);
+let version = options.version;
+if (!version) {
+	let recentTag = mostRecentTag();
+	if (recentTag) {
+		recentTag = '' + recentTag;
+		version = recentTag.trim();
+	}
 }
-if (options.tag) {
-	log(`Tagging git branch with version: ${version}`);
-	addTag(version);
+log(`Bundling Components to ${options.dest}${version}/`);
+if (version) {
+	let success = bundleComponents(version, {sourceMap: options.sourceMap, noMinify: options.noMinify});
+	if (!success) new Error('Error while bundling Components.');
+}
+else {
+	new Error('Unable to apply a version.');
 }
