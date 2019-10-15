201268199d5061fa9a4f92b00a85dd8404cd6219
Bryce Walters <bwalters@mindresearch.org>
2019-10-15 09:35:54 -0700

    WBC-10233 update the dist for modified test/bundle steps

--- dist/lib/test.js
+++ dist/lib/test.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.testGame = undefined;
+exports.testComponentBundle = exports.testGame = undefined;
 
 var _file = require('./common/file');
 
@@ -23,6 +23,35 @@ var testGame = exports.testGame = function testGame() {
             return false;
         }
     }
+    return true;
+};
+
+var testComponentBundle = exports.testComponentBundle = function testComponentBundle() {
+    // check if the package's jspm config is pointing to the correct folder for the component bundles
+    var jspmConfig = getPackageJsonField('jspm');
+    // validate existence of lib property
+    if (!jspmConfig || !jspmConfig.directories || !jspmConfig.directories.lib) {
+        console.log('Unable to identify the jspm lib for components config');
+        return false;
+    }
+    // validate lib property is correct
+    if (jspmConfig.directories.lib != 'dist') {
+        console.log('directory library folder for component bundles must be the dist folder');
+        return false;
+    }
+
+    // check that the mind object includes details on bundling components
+    var config = getPackageJsonField('mind');
+    if (!config) {
+        console.log('Missing mind section in package.json');
+        return false;
+    }
+    // validate that componentBundles exist
+    if (!config.componentBundles) {
+        console.log('Missing component bundle configuration in package.json');
+        return false;
+    }
+
     return true;
 };
 
