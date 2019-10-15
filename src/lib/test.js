3b6d29218a7a4d9557ebeedbb64bc7017bf5f8c3
Bryce Walters <bwalters@mindresearch.org>
2019-10-15 09:33:51 -0700

    WBC-10233 add minify option and a test flag

--- src/lib/test.js
+++ src/lib/test.js
@@ -19,6 +19,35 @@ export const testGame = () => {
     return true;
 };
 
+export const testComponentBundle = () => {
+    // check if the package's jspm config is pointing to the correct folder for the component bundles
+    const jspmConfig = getPackageJsonField('jspm');
+    // validate existence of lib property
+    if (!jspmConfig || !jspmConfig.directories || !jspmConfig.directories.lib) {
+        console.log('Unable to identify the jspm lib for components config');
+        return false;
+    }
+    // validate lib property is correct
+    if (jspmConfig.directories.lib != 'dist') {
+        console.log('directory library folder for component bundles must be the dist folder');
+        return false;
+    }
+
+    // check that the mind object includes details on bundling components
+    const config = getPackageJsonField('mind');
+    if (!config) {
+        console.log('Missing mind section in package.json');
+        return false;
+    }
+    // validate that componentBundles exist
+    if (!config.componentBundles) {
+        console.log('Missing component bundle configuration in package.json');
+        return false;
+    }
+
+    return true;
+}
+
 const getPackageJsonField = field => {
 	if (!getPackageJsonField.cache) {
 		getPackageJsonField.cache = getJsonFile('package.json');
